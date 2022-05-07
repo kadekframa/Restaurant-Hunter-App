5649bda8cfecbc7548b33eb48600a137e9317aa9
kadekframa <dframa19@gmail.com>
2022-05-07 23:49:32 +0800

    First Commit. Restaurant Hunter Apps Project. Keep Spirit dan tetap Semangat !

---
+++ src/scripts/index.js
@@ -0,0 +1,25 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import data from '../DATA.json';
+
+let html = "";
+data.restaurants.forEach((resto) => {
+    html += `
+        <article class="post-item">
+            <img class="post-item__thumbnail" src="${resto.pictureId}"
+                alt="Gambar suasana restoran yang berada di ${resto.city}"/>
+            <div class="city">
+            <span>${resto.city}</span>
+            </div>
+            <div class="post-item__content">
+            <p class="post-item__date">
+                Rating <a href="#" class="post-item__date__author">${resto.rating}</a>
+            </p>
+            <h1 class="post-item__title"><a href="#">${resto.name}</a></h1>
+            <p class="post-item__description">${resto.description}</p>
+            </div>
+        </article>
+    `;
+
+    document.getElementById('content-resto').innerHTML = html;
+});
