8d9786113f1e05335687ec424e366fa9a5db66c8
kadekframa <dframa19@gmail.com>
2022-05-09 16:31:56 +0800

    Restaurant Hunter Apps. Updated appearance website implementing Mobile first approach, Responsive layout, Grid layout, Accessibility. Keep Spirit, Stay foolish Stay hungry !!!

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,48 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css'
 import data from '../DATA.json';
 
+/* get data JSON */
 let html = "";
 data.restaurants.forEach((resto) => {
     html += `
         <article class="post-item">
-            <img class="post-item__thumbnail" src="${resto.pictureId}"
+            <img tabindex="0" class="post-item-thumbnail" src="${resto.pictureId}"
                 alt="Gambar suasana restoran yang berada di ${resto.city}"/>
             <div class="city">
             <span>${resto.city}</span>
             </div>
-            <div class="post-item__content">
-            <p class="post-item__date">
-                Rating <a href="#" class="post-item__date__author">${resto.rating}</a>
+            <div class="post-item-content">
+            <p class="post-item-rate">
+                Rating <a tabindex="-1" href="#" class="post-item__date__author">${resto.rating}</a>
             </p>
-            <h1 class="post-item__title"><a href="#">${resto.name}</a></h1>
-            <p class="post-item__description">${resto.description}</p>
+            <h1 class="post-item-title"><a href="#">${resto.name}</a></h1>
+            <p class="post-item-description">${resto.description}</p>
             </div>
         </article>
     `;
 
-    document.getElementById('content-resto').innerHTML = html;
+    document.getElementById('posts-restaurant').innerHTML = html;
+});
+
+
+/* navigation drawer */
+const menu = document.querySelector('#menu');
+const drawer = document.querySelector('#drawer');
+const main = document.querySelector('main');
+const hero = document.querySelector('.hero');
+
+
+menu.addEventListener('click', function(event){
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener("click", function(){
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function(){
+    drawer.classList.remove('open');
 });
